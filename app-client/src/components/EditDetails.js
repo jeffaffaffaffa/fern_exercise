39c17cf40437585ef7279cac1068753588a3edfc
[Jeff] <[jeffyche@gmail.com]>
2020-06-26 16:25:39 -0500

    navbar buttons

--- app-client/src/components/EditDetails.js
+++ app-client/src/components/EditDetails.js
@@ -1,18 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../util/MyButton';
+
 //redux
 import { connect } from 'react-redux';
 import { editUserDetails } from '../redux/actions/userActions';
+
 //mui 
-import { Tooltip } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton';
+
 //icons
 import EditIcon from '@material-ui/icons/Edit';
 
@@ -78,11 +80,11 @@ class EditDetails extends Component {
 
         return (
             <Fragment>
-                <Tooltip title="Edit details" placement="top">
-                    <IconButton onClick={this.handleOpen} className={classes.button}>
-                        <EditIcon color="primary"/>
-                    </IconButton>
-                </Tooltip>
+
+                <MyButton tip="Edit Details" onClick={this.handleOpen} btnClassName={classes.button}>
+                    <EditIcon color="primary"/>
+                </MyButton>
+                
                 <Dialog
                     open={this.state.open}
                     onClose={this.handleClose}
