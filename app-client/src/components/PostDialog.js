ae507154663438dd5b4171e4957be4fd419601d2
[Jeff] <[jeffyche@gmail.com]>
2020-07-08 21:18:46 -0500

    post popup styling

--- app-client/src/components/PostDialog.js
+++ app-client/src/components/PostDialog.js
@@ -39,6 +39,15 @@ const style = theme => ({
     closeButton: {
         position: 'absolute',
         left: '90%'
+    },
+    expandButton: {
+        position: 'absolute',
+        left: '90%'
+    },
+    spinnerDiv: {
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 50
     }
 });
 
@@ -67,7 +76,9 @@ class PostDialog extends Component {
         } = this.props;
 
         const dialogMarkup = loading ? (
-            <CircularProgress size={200}/>
+            <div className={classes.spinnerDiv}>
+                <CircularProgress size={200} thickness={2}/>
+            </div>
         ) : (
             <Grid container spacing={16}>
                 <Grid item sm={5}>
